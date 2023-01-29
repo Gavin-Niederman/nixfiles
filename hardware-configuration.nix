1711c3e34b2a775c245be039db5f1d585fb0d84e
Gavin Niederman <gavinniederman@gmail.com>
2023-01-29 00:45:43 -0800

    add(swap and hdd mounting)

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -23,7 +23,17 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  fileSystems."/mnt/hdd" = 
+    { device = "/dev/disk/by-uuid/4E66CF3466CF1C19";
+      fsType = "ntfs";
+    };
+
+  swapDevices = [
+    {
+      device = "/var/swap";
+      size = 16384;
+    }
+   ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
