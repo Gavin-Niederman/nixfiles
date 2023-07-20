10509e8434c27690642c70d474bbc53d357bfc7d
Gavin-Niederman <gavinniederman@gmail.com>
2023-07-19 23:47:01 -0700

    fix: remove empty lines from hyprland config

--- modules/nixos/development.nix
+++ modules/nixos/development.nix
@@ -1,4 +1,5 @@
 { config, pkgs, ... }:
+
 {
     config = {
         environment.systemPackages = with pkgs; [
--- modules/nixos/monitors.nix
+++ modules/nixos/monitors.nix
@@ -51,7 +51,7 @@
             if (m.workspace != null) then "workspace=${m.output},${toString m.workspace}" else "";
         mkHyprlandMonitor = m:
             "monitor=${m.output},${toString m.dimensions.x}x${toString m.dimensions.y}@${toString m.refreshRate},${toString m.offset.x}x${toString m.offset.y},${toString m.scale}";
-        monitors = (map mkHyprlandMonitor config.monitors) ++ (map mkHyprlandWorkspace config.monitors);
+        monitors = filter (s: s != "") ((map mkHyprlandMonitor config.monitors) ++ (map mkHyprlandWorkspace config.monitors));
     in {
         home-manager.sharedModules = [
             {
