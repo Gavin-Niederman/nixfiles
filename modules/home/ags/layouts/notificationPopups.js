de47183d947fd50917b4496cbf113baf93e13b0c
Gavin-Niederman <gavinniederman@gmail.com>
2024-03-11 23:18:51 -0700

    fix: allow mouse click through on notification popups

--- modules/home/ags/layouts/notificationPopups.js
+++ modules/home/ags/layouts/notificationPopups.js
@@ -5,11 +5,12 @@ export const NotificationPopups = (monitor) =>
 {
     return Widget.Window({
         monitor,
-        margin: [32, 0],
         name: 'notification-popup',
         exclusivity: "ignore",
         anchor: ["top", "right"],
+        click_through: true,
         child: Widget.Box({
+            click_through: true,
             css: "min-width: 2px; min-height: 2px;",
             children: [Widget.Box({
                 className: 'notifications',
