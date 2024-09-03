d1211d36bd22d0909252c3f2384ae342b797455a
Gavin Niederman <gavinniederman@gmail.com>
2024-09-02 19:34:55 -0700

    feat: better time formatting

--- modules/home-manager/ags/widgets/Clock.ts
+++ modules/home-manager/ags/widgets/Clock.ts
@@ -1,4 +1,12 @@
-const time = Variable('', { poll: [1000, () => new Date().toLocaleTimeString()] });
+function formatTime(date: Date) {
+    return Intl.DateTimeFormat('en-US', {
+        dateStyle: undefined,
+        timeStyle: "medium",
+        hourCycle: "h24",
+    }).format(date)
+}
+
+const time = Variable('', { poll: [1000, () => formatTime(new Date())] });
 export default function Clock() {
     return Widget.Label({ label: time.bind() });
 }
