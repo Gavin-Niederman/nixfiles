91b4bd5f02e36784cf667c93b7212e62e451aca0
Gavin Niederman <gavinniederman@gmail.com>
2024-09-21 00:20:14 -0700

    feat: correctly assign outputs to monitors Fuck you gdk why can you not just make this easy

--- modules/home-manager/ags/main.ts
+++ modules/home-manager/ags/main.ts
@@ -6,11 +6,22 @@ import {
 
 App.addIcons(App.configDir + '/assets');
 
-const numMonitors = Gdk.Display.get_default()?.get_n_monitors() || 1;
-const monitors = Array.from({ length: numMonitors }, (_, i) => i);
+const display = Gdk.Display.get_default();
+display?.get_screen
+const numMonitors = display?.get_n_monitors() || 1;
+const monitors = Array.from(
+    { length: numMonitors },
+    (_, i) => ({
+        monitor: i,
+        output: Object.entries(Niri.outputs)
+            .find(
+                (o) => o[1].model === display?.get_monitor(i)?.get_model()
+            )?.[0] || undefined
+    })
+);
 
-function forEachMonitor<T>(generator: (monitor: number) => T): T[] {
-    return monitors.map(generator);
+function forEachMonitor<T>(generator: (monitor: number, output: string | undefined) => T): T[] {
+    return monitors.map(({ monitor, output }) => generator(monitor, output));
 }
 
 Style.connect("style-rebuilt", (service) => {
@@ -21,7 +32,7 @@ App.config({
     // generated in config.js
     style: "/tmp/ags/style.css",
     windows: [
-        ...forEachMonitor((monitor) => SideBar(monitor, Object.keys(Niri.outputs)[monitor])),
+        ...forEachMonitor((monitor, output) => SideBar(monitor, output!)),
         ...forEachMonitor((monitor) => ScreenBevels(monitor)),
         ...forEachMonitor((monitor) => ClockUnderlay(monitor)),
         QuickSettings()
